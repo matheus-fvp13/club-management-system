0a8962c78dd557e953ee7dae2d486c0db1fef6e2
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2023-10-17 21:39:40 -0300

    project created

---
+++ src/main/java/edu/mfvp/sgc/SgcApplication.java
@@ -0,0 +1,13 @@
+package edu.mfvp.sgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcApplication.class, args);
+	}
+
+}
